4b2479ad868e2bc65e04283523bdb9725a48f08e
JinweiFang <Jinwei@uwm.edu>
2022-09-27 14:59:43 -0500

    modified the models.py

--- App/models.py
+++ App/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
 # Create your models here.
+ROLES = (
+    ("Admin", "Admin"),
+    ("User", "User")
+)
 
 
 class MyUser(models.Model):
@@ -9,3 +13,17 @@ class MyUser(models.Model):
     first_name = models.CharField(max_length=20, default=None)
     email = models.CharField(max_length=30, default=None)
     password = models.CharField(max_length=64, default=None)
+    role = models.CharField(max_length=10, choices=ROLES)
+
+    def str(self):
+        return self.username
+
+
+class Campaign(models.Model):
+    campaignName = models.CharField(max_length=20)
+    campaignCode = models.CharField(max_length=20)
+    owner = models.CharField(max_length=20)
+
+    def str(self):
+        return self.campaignName
+
