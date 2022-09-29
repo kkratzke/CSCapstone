2f946a7c73f6986a9b9122f70d573a74ab9fcbbe
JinweiFang <Jinwei@uwm.edu>
2022-09-29 14:11:19 -0500

    modified the models.py

--- App/models.py
+++ App/models.py
@@ -3,7 +3,22 @@ from django.db import models
 # Create your models here.
 ROLES = (
     ("Admin", "Admin"),
-    ("User", "User")
+    ("User", "User"),
+)
+
+TYPES = (
+    ("Medical", "Medical"),
+    ("Memorial", "Memorial"),
+    ("Emergency", "Emergency"),
+    ("Education", "Education"),
+    ("Other", "Other"),
+)
+
+STATUS = (
+    ("On going", "On going"),
+    ("Suspended", "Suspended"),
+    ("Canceled", "Canceled"),
+    ("End", "End"),
 )
 
 
@@ -12,16 +27,18 @@ class MyUser(models.Model):
     last_name = models.CharField(max_length=20, default=None)
     first_name = models.CharField(max_length=20, default=None)
     email = models.CharField(max_length=30, default=None)
+    role = models.CharField(max_length=10, choices=ROLES, default="User")
     password = models.CharField(max_length=64, default=None)
-    role = models.CharField(max_length=10, choices=ROLES)
 
     def str(self):
         return self.username
 
 
 class Campaign(models.Model):
-    campaignName = models.CharField(max_length=20)
+    campaignName = models.CharField(max_length=150)
+    type = models.CharField(max_length=15, choices=TYPES, default="Other")
     campaignCode = models.CharField(max_length=20)
+    status = models.CharField(max_length=15, choices=STATUS, default="On going")
     owner = models.CharField(max_length=20)
 
     def str(self):
