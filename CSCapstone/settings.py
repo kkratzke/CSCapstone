7b1c5b67da1ac170355cf3aac8623587bef959dc
Terence Lee <tklee@uwm.edu>
2022-10-05 22:47:08 -0500

    Cloud database functionality added

--- CSCapstone/settings.py
+++ CSCapstone/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-
+import MySQLdb
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -77,8 +77,15 @@ WSGI_APPLICATION = 'CSCapstone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': 'appusers.clpjfhtuhjxg.us-east-2.rds.amazonaws.com',
+        'PORT': 3306,
+        'NAME': 'UserAccounts',
+        'USER': 'admin',
+        'PASSWORD': 'TestDev1!',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
