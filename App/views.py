2f0d50d7c863ca6e83a7bd3162a7feacfef3be71
Terence Lee <tklee@uwm.edu>
2022-10-06 21:08:11 -0500

    Update project from master

--- App/views.py
+++ App/views.py
@@ -17,6 +17,9 @@ class Homescreen(View):
         if request.method == 'POST' and 'login_page' in request.POST:
             return render(request, "Login.html")
 
+        if request.method == 'POST' and 'logout' in request.POST:
+            return redirect('/', request)
+
         if request.method == 'POST' and 'create_account_page' in request.POST:
             return render(request, "CreateAccount.html")
 
@@ -25,17 +28,17 @@ class Homescreen(View):
                 return render(request, "Login.html", {"message": "Incorrect Login credentials"})
             else:
                 request.session['login'] = request.POST['uname']
-                return render(request, "Homescreen.html", {})
+                return render(request, "Homescreen.html", {"login": request.session['login']})
 
         if request.method == 'POST' and "create_account_button" in request.POST:
             ret = create_account(request.POST['uname'], request.POST['email'], request.POST['first_name'],
-                           request.POST['last_name'], request.POST['password'], request.POST['password2'])
+                                 request.POST['last_name'], request.POST['password'], request.POST['password2'])
             message = ret[0]
             reload_content = ret[1]
             if message != "":
                 return render(request, "CreateAccount.html", {"message": message, "reload_content": reload_content})
             else:
-                return redirect("/landing/", request)
+                return redirect("/pagejump/", request)
 
         if request.method == 'POST' and 'create_campaign_page' in request.POST:
             return render(request, "CreateCampaign.html")
@@ -52,26 +55,49 @@ class Homescreen(View):
                 owner = MyUser.objects.get(username__iexact=request.session['login'])
                 newCampaign = Campaign(campaignName=campaignName, type=type, campaignCode=campaignCode, owner=owner)
                 newCampaign.save()
-                return render(request, "Homescreen.html")
+                return render(request, "Homescreen.html", {"login": request.session['login']})
 
         if request.method == 'POST' and 'edit_profile_page' in request.POST:
-            owner = MyUser.objects.get(username__iexact=request.session['login'])
-            reload_content = [owner.email, owner.first_name, owner.last_name, "", ""]
-            return render(request, "Profile.html", {"reload_content": reload_content})
+            logged_in = request.session['login']
+            if logged_in is None:
+                # need to deny access to edit account
+                return render(request, "Profile.html", {"message": "Must be logged in to edit profile"})
+            else:
+                owner = MyUser.objects.get(username__iexact=request.session['login'])
+                reload_content = [owner.email, owner.first_name, owner.last_name, "", ""]
+                return render(request, "Profile.html", {"reload_content": reload_content})
 
         if request.method == 'POST' and "edit_profile_button" in request.POST:
             owner = MyUser.objects.get(username__iexact=request.session['login'])
-            message = edit_profile(request.POST['email'], request.POST['first_name'], request.POST['last_name'], request.POST['password'], request.POST['password2'], request.session['login'])
+            message = edit_profile(request.POST['email'], request.POST['first_name'], request.POST['last_name'],
+                                   request.POST['password'], request.POST['password2'], request.session['login'])
             reload_content = [owner.email, owner.first_name, owner.last_name, "", ""]
             if message != "":
                 return render(request, "Profile.html", {"message": message, "reload_content": reload_content})
             else:
-                return redirect("/landing/", request)
+                return render(request, "Homescreen.html", {"login": request.session['login']})
 
 
 class Landing(View):
     def get(self, request):
         return render(request, "Landing.html", {})
+
+    def post(selfself, request):
+        return redirect("/", request)
+class LogIn(View):
+    def get(self, request):
+        return render(request, "Login.html", {})
+    def post(self, request):
+        if request.method == 'POST' and 'create_account_page' in request.POST:
+            return redirect("/createaccount/")
+class CreateAccount(View):
+    def get(self, request):
+        return render(request, "CreateAccount.html")
+
+
+class PageJump(View):
+    def get(self, request):
+        return render(request, "PageJump.html", {})
 
     def post(selfself, request):
         return redirect("/", request)
