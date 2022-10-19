90f021d726c4c60dbbb759ed33dda239a60b1bed
Cal Schmitt <schm2455@uwm.edu>
2022-10-18 22:53:06 -0500

    Navbar updates, create campaign UI update

--- App/views.py
+++ App/views.py
@@ -17,6 +17,9 @@ class Homescreen(View):
         if request.method == 'POST' and 'login_page' in request.POST:
             return render(request, "Login.html")
 
+        if request.method == 'POST' and 'logout' in request.POST:
+            return redirect('/', request)
+
         if request.method == 'POST' and 'create_account_page' in request.POST:
             return render(request, "CreateAccount.html")
 
@@ -25,7 +28,7 @@ class Homescreen(View):
                 return render(request, "Login.html", {"message": "Incorrect Login credentials"})
             else:
                 request.session['login'] = request.POST['uname']
-                return render(request, "Homescreen.html", {})
+                return render(request, "Homescreen.html", {"login": request.session['login']})
 
         if request.method == 'POST' and "create_account_button" in request.POST:
             ret = create_account(request.POST['uname'], request.POST['email'], request.POST['first_name'],
@@ -39,6 +42,9 @@ class Homescreen(View):
 
         if request.method == 'POST' and 'create_campaign_page' in request.POST:
             return render(request, "CreateCampaign.html")
+        
+        if request.method == 'POST' and 'my_campaigns' in request.POST:
+            return render(request, "MyCampaigns.html")
 
         if request.method == 'POST' and "create_campaign" in request.POST:
             logged_in = request.session['login']
@@ -48,11 +54,28 @@ class Homescreen(View):
             else:
                 campaignName = request.POST['campaign_name']
                 type = request.POST['type']
-                campaignCode = request.POST['campaign_code']
-                owner = MyUser.objects.get(username__iexact=request.session['login'])
-                newCampaign = Campaign(campaignName=campaignName, type=type, campaignCode=campaignCode, owner=owner)
+                highest_code_campaign = Campaign.objects.all().order_by('-id')[:1].first()
+                highest_code = highest_code_campaign.campaignCode + 1
+                owner = request.session['login']
+                newCampaign = Campaign(campaignName=campaignName, type=type, campaignCode=highest_code, owner=owner)
                 newCampaign.save()
-                return render(request, "Homescreen.html")
+                return render(request, "Homescreen.html", {"login": request.session['login']})
+
+        if request.method == 'POST' and 'my_campaigns' in request.POST:
+            # make the MyCampaigns html appear - need to pass the logged-in user's campaigns
+            my_campaigns = Campaign.objects.get(owner__iexact=request.session['login'])
+            return render(request, {'campaigns': my_campaigns})
+
+
+        if request.method == 'POST' and "delete_campaign" in request.POST:
+            campaignId = request.POST['campaignId']
+            campaign = Campaign.objects.get(id__iexact=campaignId)
+            campaign.delete()
+
+            my_campaigns = Campaign.objects.get(owner__iexact=request.session['login'])
+
+            # make the MyCampaigns html appear again
+            return render({'campaigns': my_campaigns})
 
         if request.method == 'POST' and 'edit_profile_page' in request.POST:
             logged_in = request.session['login']
@@ -72,7 +95,7 @@ class Homescreen(View):
             if message != "":
                 return render(request, "Profile.html", {"message": message, "reload_content": reload_content})
             else:
-                return render(request, "Homescreen.html")
+                return render(request, "Homescreen.html", {"login": request.session['login']})
 
 
 class Landing(View):
